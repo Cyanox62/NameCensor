ac901768e656f3eee13ea50a2635fe44870dc763
Cyanox62 <infernogaming621@gmail.com>
2019-05-03 21:52:32 -0400

    Initla commit

---
+++ NameCensor/EventHandler.cs
@@ -0,0 +1,44 @@
+﻿using Smod2.EventHandlers;
+using Smod2.Events;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text.RegularExpressions;
+
+namespace NameCensor
+{
+	class EventHandler :IEventHandlerPlayerJoin, IEventHandlerWaitingForPlayers
+	{
+		private Plugin instance;
+		private List<string> bList;
+
+		public EventHandler(Plugin instance)
+		{
+			this.instance = instance;
+		}
+
+		private void LoadConfigs()
+		{
+			bList = instance.GetConfigList("nc_blacklist").ToList();
+		}
+
+		public void OnWaitingForPlayers(WaitingForPlayersEvent ev)
+		{
+			LoadConfigs();
+		}
+
+		public void OnPlayerJoin(PlayerJoinEvent ev)
+		{
+			NicknameSync sync = ((GameObject)ev.Player.GetGameObject()).GetComponent<NicknameSync>();
+			string name = sync.NetworkmyNick;
+			foreach (string str in bList.Select(x => x = x.ToLower().Trim()))
+			{
+				for (int i = 0; i < Regex.Matches(name.ToLower(), str).Count; i++)
+				{
+					name = Regex.Replace(name, str, new string('-', str.Length), RegexOptions.IgnoreCase);
+				}
+			}
+			sync.UpdateNickname(name);
+		}
+	}
+}
---
+++ NameCensor/Plugin.cs
@@ -0,0 +1,28 @@
+﻿using Smod2.Attributes;
+
+namespace NameCensor
+{
+	[PluginDetails(
+	author = "Cyanox",
+	name = "NameCensor",
+	description = "Censors names.",
+	id = "cyan.namecensor",
+	version = "1.0.0",
+	SmodMajor = 3,
+	SmodMinor = 0,
+	SmodRevision = 0
+	)]
+	public class Plugin : Smod2.Plugin
+	{
+		public override void OnDisable() { }
+
+		public override void OnEnable() { }
+
+		public override void Register()
+		{
+			AddConfig(new Smod2.Config.ConfigSetting("nc_blacklist", new string[]{}, false, true, "The words to be censored."));
+
+			AddEventHandlers(new EventHandler(this));
+		}
+	}
+}
